5054998e5335dcfd6785894a67ee6a938f34d640
Ilmari-Lindberg <ilmari.lindberg@gmail.com>
2021-03-24 14:41:53 +0200

    ISOJA muutoksia (JS/HTML). Tarvitaan yksi välilyönti, jotta nimi hyväksytään. Ikä ja sähköposti kysymys. Myös jotain muuta, jota en muista :D // Ilmari // 24.3.2021

--- Projektit/LomakeJS.js
+++ Projektit/LomakeJS.js
@@ -1,31 +1,58 @@
 function nappula(){
   var nimi = document.getElementById("nimi").value;
   var salasana = document.getElementById("salasana").value;
+  var email = document.getElementById("email").value;
+  var ika = document.getElementById("ika").value;
 
-//Nimen tarkistus
+//1 Nimen tarkistus
+var ehdot = /^[a-zA-Z]+\s[a-zA-Z]+$/;
   if(nimi == "") {
-  console.log("Kirjoita nimesi.");
+    alert("Kirjoita nimesi.");
   }
 
+//1 Nimen välilyönnit
+  else if (!ehdot.test(nimi)) {
+    alert("Kirjoita etu- ja sukunimi, vain yksi välilyönti niiden välissä.")
+  }
+
+//1 Nimen pituus
   else if (nimi.length < 8) {
-    console.log("Nimesi on liian lyhyt.");
+    alert("Nimesi on liian lyhyt. Sen täytyy olla vähintään 8-merkkiä pitkä.");
   }
   else {
-  console.log("Nimi: OK")
+    console.log("Nimi: OK");
   }
-
-//Salasanan tarkistus
+// ---------------------------
+//2 Salasanan tarkistus
   if(salasana  == "") {
-  console.log("Kirjoita Salasana.");
+    alert("Kirjoita Salasana.");
   }
-
-  else if (salasana.length < 12) {
-    console.log("Salasana on liian lyhyt.");
+//2 Salasanan pituus
+  else if (salasana.length < 10) {
+    alert("Salasana on liian lyhyt. Sen täytyy olla vähintään 12-merkkiä pitkä.");
   }
 
   else {
-  console.log("Salasana: OK")
+    console.log("Salasana: OK");
   }
 
+// ---------------------------
+//3 Sähköpostin tarkistus
+var ehdot = /^.+@.+\..+$/;
+  if (!ehdot.test(email)) {
+    alert("Sähköpostisi on virheellisessä muodossa.");
+  }
+  else {
+    console.log("Email: OK");
+  }
 
+// ---------------------------
+//4 Iän numeron tarkistus
+var ehdot = /\d/;
+  if (!ehdot.test(ika))
+  alert("Ikä pitää kirjoittaa numeroina.")
+
+  else {
+    console.log("Ikä: OK");
+  }
 }

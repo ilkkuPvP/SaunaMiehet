f88f89b6c51dfcdf364803a7c55eb99b158bf1b0
Ilmari-Lindberg <ilmari.lindberg@gmail.com>
2021-03-24 12:58:31 +0200

    Kertoo kun salasana tai nimi on liian lyhyt (JS) // Ilmari // 24.3.2021

--- Projektit/LomakeJS.js
+++ Projektit/LomakeJS.js
@@ -2,16 +2,25 @@ function nappula(){
   var nimi = document.getElementById("nimi").value;
   var salasana = document.getElementById("salasana").value;
 
+//Nimen tarkistus
   if(nimi == "") {
   console.log("Kirjoita nimesi.");
   }
 
+  else if (nimi.length < 8) {
+    console.log("Nimesi on liian lyhyt.");
+  }
   else {
   console.log("Nimi: OK")
   }
 
+//Salasanan tarkistus
   if(salasana  == "") {
   console.log("Kirjoita Salasana.");
+  }
+
+  else if (salasana.length < 12) {
+    console.log("Salasana on liian lyhyt.");
   }
 
   else {

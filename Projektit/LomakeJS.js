df47cbad852d0d2a485cb8e0b36f906ad16dd2dd
Samppa245 <Samu.hamalainen@edu.keuda.fi>
2021-03-24 11:42:41 +0200

    nappula ei toimi olkulle // Samu hämäläinen // 24.3.2021

--- Projektit/LomakeJS.js
+++ Projektit/LomakeJS.js
@@ -2,14 +2,15 @@ function Nappula(){
 var nimi = document.getElementById("Nimi").value;
 var salasana = document.getElementById("Salasana").value;
 
+if(nimi === true){
+console.log("Toimii");
+}else{
+console.log("laita kenttään asioita saatana")
 
 
 
 
-
-
-
-
+}
 
 
 }

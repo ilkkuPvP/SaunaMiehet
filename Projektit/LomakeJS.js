7e15521c7a20c6605217313372cbce2f96b4d751
Samppa245 <Samu.hamalainen@edu.keuda.fi>
2021-03-24 16:04:24 +0200

    Loppu viimeistely eli kohtien 5, 6 ja 7  tekeminen// Samu // 24.3.2021

--- Projektit/LomakeJS.js
+++ Projektit/LomakeJS.js
@@ -3,6 +3,13 @@ function nappula(){
   var salasana = document.getElementById("salasana").value;
   var email = document.getElementById("email").value;
   var ika = document.getElementById("ika").value;
+  var pelaaja = document.getElementById("pelaaja").checked;
+  var pelaajav2 = document.getElementById("opiskeluun").checked;
+  var pelaajav3 = document.getElementById("eiKonetta").checked;
+  var mielipide = document.getElementById("tosiHieno").checked;
+  var mielipide2 = document.getElementById("ammattimainen").checked;
+  var mielipide3 = document.getElementById("ostan").checked;
+  var lempiaine = document.getElementById("lempiAine").value;
 
 //1 Nimen tarkistus
 var ehdot = /^[a-zA-Z]+\s[a-zA-Z]+$/;
@@ -55,4 +62,37 @@ var ehdot = /\d/;
   else {
     console.log("Ikä: OK");
   }
+// -----------------------------------------------------
+//5. minkälainen koneen käyttäjä olet olet tarkistus
+if(pelaaja === true){
+console.log("koneen käyttäjä on ok")
+} else if (pelaajav2 === true){
+console.log("koneen käyttäjä on ok")
+} else if (pelaajav3 === true) {
+console.log("Koneen käyttäjä on ok")
+} else {
+alert("Valitse minkälainen koneen käyttäjä olet.")
+}
+// -----------------------------------
+//6. mitä mieltä olet sivustamme
+if(mielipide === true){
+console.log("Mielipide ok")
+
+}if(mielipide2 === true){
+console.log("Mielipidev2 ok")
+
+}if(mielipide3 === true){
+console.log("Mielipidev3 ok")
+
+} else if ((mielipide === false) && (mielipide2 === false) && (mielipide2 === false)) {
+alert("Valitse mielipiteesi sivustamme.")
+
+} // -------------------------
+  //7. Lempiaine tarkistus
+
+  if(lempiaine == "empty"){
+  alert("Valitse valikosta lempi aineesi.")
+} else {
+console.log("Lempiaine ok");
+}
 }

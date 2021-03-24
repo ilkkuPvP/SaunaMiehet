d7e146de6d425a88d0c869fbb19ccb714e173d1f
Samppa245 <Samu.hamalainen@edu.keuda.fi>
2021-03-24 12:00:35 +0200

    random välimuokkauksia // Samu Hämäläinen // 24.3.2021

--- Projektit/LomakeJS.js
+++ Projektit/LomakeJS.js
@@ -1,11 +1,12 @@
-function Nappula(){
+function Nappula()
+{
 var nimi = document.getElementById("Nimi").value;
 var salasana = document.getElementById("Salasana").value;
 
 if(nimi === true){
 console.log("Toimii");
 }else{
-console.log("laita kenttään asioita saatana")
+console.log("laita kenttään asioita saatana");
 
 
 

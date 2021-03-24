f3aa6466da3204749f79d01710915b43155b83b9
Ilmari-Lindberg <ilmari.lindberg@gmail.com>
2021-03-24 12:10:25 +0200

    Korjasin napin (JS)! Lisäsin kommentin (HTML) (JS) // Ilmari Lindberg // 24.3.2021

--- Projektit/LomakeJS.js
+++ Projektit/LomakeJS.js
@@ -1,20 +1,10 @@
-function Nappula()
-{
-var nimi = document.getElementById("Nimi").value;
-var salasana = document.getElementById("Salasana").value;
-
-if(nimi === true){
-console.log("Toimii");
-}else{
-console.log("laita kenttään asioita saatana");
-=======
+function nappula(){
   var nimi = document.getElementById("Nimi").value;
   var salasana = document.getElementById("Salasana").value;
 
   if(nimi == "") {
   console.log("laita kenttään asioita saatana!");
   }
->>>>>>> 95e28cb8d4ccf658aefa61ef01a6e3f36d17a4dd
 
   else {
   console.log("Hieno nimi!")

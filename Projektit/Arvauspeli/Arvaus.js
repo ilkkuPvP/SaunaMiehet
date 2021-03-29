0f04a666a24cea6e00d8ab03edc9edae889e3183
Samppa245 <Samu.hamalainen@edu.keuda.fi>
2021-03-29 11:23:38 +0300

    Arvonta kone // Samu // 29.3.2021

--- Projektit/Arvauspeli/Arvaus.js
+++ Projektit/Arvauspeli/Arvaus.js
@@ -1,7 +1,8 @@
-function Nappula(){
 
 
+// arvonta kone
 
 
-
-}
+var numerot = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var arvonta = Math.floor(Math.random() * numerot.length);
+console.log(arvonta);

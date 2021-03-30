d73eea9e2449c0e7469ac7be88aafbe5e8899ff8
Samppa245 <Samu.hamalainen@edu.keuda.fi>
2021-03-30 14:04:43 +0300

    arvontapeli, joka osaa laskea isompi, yhtäsuuri tai pienempi // samu // 30.3.2021

--- Projektit/Arvauspeli/Arvaus.js
+++ Projektit/Arvauspeli/Arvaus.js
@@ -1,8 +1,116 @@
 
 
 // arvonta kone
-
-
 var numerot = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-var arvonta = Math.floor(Math.random() * numerot.length);
+var arvonta = Math.floor(Math.random() * numerot.length + 1);
 console.log(arvonta);
+
+
+
+function Nappula(){
+
+  arvov1 = document.getElementById("nappiv1").value;
+  if(arvov1 < arvonta){
+  console.log("suurempi");
+
+} else if (arvov1 > arvonta) {
+  console.log("pienempi");
+
+} else if (arvov1 == arvonta){
+    console.log("oikein");
+  }
+}
+
+function Nappula2(){
+
+  arvov2 = document.getElementById("nappiv2").value;
+
+  if(arvov2 < arvonta){
+  console.log("suurempi");
+} else if (arvov2 > arvonta) {
+  console.log("pienempi");
+} else if (arvov2 == arvonta){
+    console.log("oikein");
+  }
+}
+function Nappula3(){
+  arvov3 = document.getElementById("nappiv3").value;
+  if(arvov3 < arvonta){
+  console.log("suurempi")
+} else if (arvov3 > arvonta) {
+  console.log("pienempi");
+} else if (arvov3 == arvonta){
+    console.log("oikein");
+  }
+}
+function Nappula4(){
+  arvov4 = document.getElementById("nappiv4").value;
+  if(arvov4 < arvonta){
+  console.log("suurempi")
+} else if (arvov4 > arvonta) {
+  console.log("pienempi");
+} else if (arvov4 == arvonta){
+    console.log("oikein");
+  }
+}
+
+function Nappula5(){
+  arvov5 = document.getElementById("nappiv5").value;
+  if(arvov5 < arvonta){
+  console.log("suurempi")
+} else if (arvov5 > arvonta) {
+  console.log("pienempi");
+} else if (arvov5 == arvonta){
+    console.log("oikein");
+  }
+}
+function Nappula6(){
+  arvov6 = document.getElementById("nappiv6").value;
+  if(arvov6 < arvonta){
+  console.log("suurempi")
+} else if (arvov6 > arvonta) {
+  console.log("pienempi");
+} else if (arvov6 == arvonta){
+    console.log("oikein");
+  }
+}
+function Nappula7(){
+  arvov7 = document.getElementById("nappiv7").value;
+  if(arvov7 < arvonta){
+  console.log("suurempi")
+} else if (arvov7 > arvonta) {
+  console.log("pienempi");
+} else if (arvov7 == arvonta){
+    console.log("oikein");
+  }
+}
+function Nappula8(){
+  arvov8 = document.getElementById("nappiv8").value;
+  if(arvov8 < arvonta){
+  console.log("suurempi")
+} else if (arvov8 > arvonta) {
+  console.log("pienempi");
+} else if (arvov8 == arvonta){
+    console.log("oikein");
+  }
+}
+function Nappula9(){
+  arvov9 = document.getElementById("nappiv9").value;
+  if(arvov9 < arvonta){
+  console.log("suurempi")
+} else if (arvov9 > arvonta) {
+  console.log("pienempi");
+} else if (arvov9 == arvonta){
+    console.log("oikein");
+  }
+}
+function Nappula10(){
+  arvov10 = document.getElementById("nappiv10").value;
+  if(arvov10 < arvonta){
+  console.log("suurempi")
+} else if (arvov10 > arvonta) {
+  console.log("pienempi");
+} else if (arvov10 == arvonta){
+    console.log("oikein");
+  }
+}

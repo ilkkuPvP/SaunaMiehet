2dad17966969f3ae725e8d837f4040769f5ab353
Ilmari-Lindberg <ilmari.lindberg@gmail.com>
2021-03-30 14:51:02 +0300

    Vaihdoin .logit alerteiksi ja paransin äidinkieltä. // Ilmari // 30.3.2021

--- Projektit/Arvauspeli/Arvaus.js
+++ Projektit/Arvauspeli/Arvaus.js
@@ -11,13 +11,13 @@ function Nappula(){
 
   arvov1 = document.getElementById("nappiv1").value;
   if(arvov1 < arvonta){
-  console.log("suurempi");
+  alert("Luku on suurempi.");
 
 } else if (arvov1 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 
 } else if (arvov1 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 
@@ -26,91 +26,91 @@ function Nappula2(){
   arvov2 = document.getElementById("nappiv2").value;
 
   if(arvov2 < arvonta){
-  console.log("suurempi");
+  alert("Luku on suurempi.");
 } else if (arvov2 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov2 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 function Nappula3(){
   arvov3 = document.getElementById("nappiv3").value;
   if(arvov3 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov3 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov3 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 function Nappula4(){
   arvov4 = document.getElementById("nappiv4").value;
   if(arvov4 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov4 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov4 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 
 function Nappula5(){
   arvov5 = document.getElementById("nappiv5").value;
   if(arvov5 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov5 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov5 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 function Nappula6(){
   arvov6 = document.getElementById("nappiv6").value;
   if(arvov6 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov6 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov6 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 function Nappula7(){
   arvov7 = document.getElementById("nappiv7").value;
   if(arvov7 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov7 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov7 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 function Nappula8(){
   arvov8 = document.getElementById("nappiv8").value;
   if(arvov8 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov8 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov8 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 function Nappula9(){
   arvov9 = document.getElementById("nappiv9").value;
   if(arvov9 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov9 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov9 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }
 function Nappula10(){
   arvov10 = document.getElementById("nappiv10").value;
   if(arvov10 < arvonta){
-  console.log("suurempi")
+  alert("Luku on suurempi.")
 } else if (arvov10 > arvonta) {
-  console.log("pienempi");
+  alert("Luku on pienempi.");
 } else if (arvov10 == arvonta){
-    console.log("oikein");
+    alert("Hienoa! Arvasit oikein! Vai huijasitko?");
   }
 }

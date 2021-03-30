a9251f826bb591c19ca9de68bf11b27132d1b036
Ilmari-Lindberg <ilmari.lindberg@gmail.com>
2021-03-30 14:46:58 +0300

    Poistin Arvaus2.js koodit, jotta Arvaus.js toimisi // Ilmari // 30.3.2021

--- Projektit/Arvauspeli/Arvaus2.js
+++ Projektit/Arvauspeli/Arvaus2.js
@@ -1,4 +1,4 @@
-function arvonta() {
+/*function arvonta() {
   var numerot = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
   var arvottu = Math.floor(Math.random() * numerot.length) + 1;
   console.log(arvottu);
@@ -9,7 +9,7 @@ arvonta();
 function Nappula() {
 
 }
-
+*/
 
 
 

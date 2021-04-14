cf884a99599785ad3308dd25d586cdc2399fb445
Ilmari-Lindberg <ilmari.lindberg@gmail.com>
2021-04-14 09:56:17 +0300

    Poistin turhan Var:n // Ilmari // 14.4.2021

--- Projektit/Arvauspeli/Arvaus.js
+++ Projektit/Arvauspeli/Arvaus.js
@@ -2,7 +2,7 @@
 
 
 // arvonta kone
-var vHistoria = 0, vHistoriaText = "Voitot: ";
+var vHistoria = 0;
 
 var numerot = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 var arvonta = Math.floor(Math.random() * numerot.length + 1);

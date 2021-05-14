d30288eb4df2c405a1075fe6564243dbe015cc8b
0lkku <oliver.finnlund@gmail.com>
2021-05-14 10:45:26 +0300

    korjailuja // Oliver // 14.5.2021

--- Projektit/Tehtavalista/lista.js
+++ Projektit/Tehtavalista/lista.js
@@ -1,56 +1,57 @@
-var myNodeList = document.getElementsByTagName('LI')
+
+var myNodeList = document.getElementsByTagName('LI');
 
 var i;
 
-for (i=0; i<myNodeList.length; i++) {
+for (i = 0; i < myNodeList.length; i++) {
 
-var span = document.createElement('SPAN')
+var span = document.createElement("SPAN");
 var txt = document.createTextNode('\u00D7');
-span.className='close'
+span.className="close";
 span.appendChild(txt);
-myNodeList[i].appendChild(span)
+myNodeList[i].appendChild(span);
 }
 
 //Tällä poistetaan joku tehtävä
-var close = document.getElementsByClassName('close')
+var close = document.getElementsByClassName("close");
 var i;
 
-for(i=0; i<close.length; i++){
-  close[i].onclick=function(){
+for(i = 0; i < close.length; i++){
+  close[i].onclick = function() {
     var div = this.parentElement;
-    div.style.display="none"
+    div.style.display = "none";
   }
 }
-var list = document.querySelector('ul')
-list.addEventListener('click' ,function(ev){
-  if(ev.target.tagName === 'LI') {
-  ev.target.classList.toggle('checked')
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+  if (ev.target.tagName === 'LI') {
+  ev.target.classList.toggle('checked');
   }
 },false)
 
 function newElement() {
-var li = document.createElement('li')
-var inputValue = document.getElementById('item').value
-var t = document.createTextNode(inputValue)
-li.appendChild(t)
+var li = document.createElement('li');
+var inputValue = document.getElementById('item').value;
+var t = document.createTextNode(inputValue);
+li.appendChild(t);
 if(inputValue === "") {
-  alert('Tämä kenttä ei saa olla tyhjä')
+  alert('Tämä kenttä ei saa olla tyhjä');
 }else {
-  document.getElementById('list').appendChild(li)
+  document.getElementById('list').appendChild(li);
 
 
 }
-document.getElementById('item').value=''
+document.getElementById("item").value='';
 
-var span= document.createElement('SPAN');
-var txt= document.createTextNode('\u00D7')
-span.appendChild(txt)
-li.appendChild(span)
+var span = document.createElement("SPAN");
+var txt = document.createTextNode('\u00D7');
+span.appendChild(txt);
+li.appendChild(span);
 
-for (i=0;i<close.length;i++){
-  close[i].onclick=function(){
-    var div= this.parentElement;
-    div.style.display="none"
+for (i = 0; i < close.length; i++){
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.display = "none";
   }
 }
 }
